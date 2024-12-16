bb3156b31a22a027ea8d7b82f385d54875743c31
Stephen Wheet <stephenwheet@gmail.com>
2024-12-16 16:29:55 +0000

    changes

--- useful-files/enablefplforwarding.sh
+++ useful-files/enablefplforwarding.sh
@@ -1,11 +1,5 @@
 nohup sudo aws ssm start-session \
 --target i-0a28f48f98341b58d \
 --document-name AWS-StartPortForwardingSessionToRemoteHost \
---parameters '{"portNumber":["443"],"localPortNumber":["443"],"host":["console-openshift-console.apps.ocp-sandbox.non-prod.mas9.nexteramaximo.ibm.com"]}' \
---profile FPL-MAS9-NONPROD &
-
-nohup sudo aws ssm start-session \
---target i-0a28f48f98341b58d \
---document-name AWS-StartPortForwardingSessionToRemoteHost \
 --parameters '{"portNumber":["6443"],"localPortNumber":["6443"],"host":["api.ocp-sandbox.non-prod.mas9.nexteramaximo.ibm.com"]}' \
 --profile FPL-MAS9-NONPROD &
--- useful-files/killfplforwarding.sh
+++ useful-files/killfplforwarding.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Kill all nohup processes related to AWS SSM port forwarding
-ps aux | grep '[n]ohup sudo aws ssm start-session' | awk '{print $2}' | xargs sudo kill
+ps aux | grep 'sudo aws ssm start-session' | awk '{print $2}' | xargs sudo kill

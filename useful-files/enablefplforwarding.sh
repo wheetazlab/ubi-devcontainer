b018e3bdd5c898f09a8302f36c03d081527a4f03
Stephen Wheet <stephenwheet@gmail.com>
2024-12-19 07:31:06 -0800

    changes

--- useful-files/enablefplforwarding.sh
+++ useful-files/enablefplforwarding.sh
@@ -1,5 +1,5 @@
-nohup sudo aws ssm start-session \
+sudo aws ssm start-session \
 --target i-0a28f48f98341b58d \
 --document-name AWS-StartPortForwardingSessionToRemoteHost \
---parameters '{"portNumber":["6443"],"localPortNumber":["6443"],"host":["api.ocp-sandbox.non-prod.mas9.nexteramaximo.ibm.com"]}' \
---profile FPL-MAS9-NONPROD &
+--parameters '{"portNumber":["6443"],"localPortNumber":["6443"],"host":["api.ocp.non-prod.nexteramaximo.ibm.com"]}' \
+--profile FPL-MAS9-NONPROD

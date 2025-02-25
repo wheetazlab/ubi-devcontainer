78938857426b16040ed6556f1c44d7ca6422c023
Stephen Wheet <stephenwheet@gmail.com>
2025-02-24 20:57:59 -0800

    updated

---
+++ useful-files/sandbox-enablefplforwarding.sh
@@ -0,0 +1,5 @@
+sudo aws ssm start-session \
+--target i-0a28f48f98341b58d \
+--document-name AWS-StartPortForwardingSessionToRemoteHost \
+--parameters '{"portNumber":["6443"],"localPortNumber":["6443"],"host":["api.ocp.sandbox.nexteramaximo.ibm.com"]}' \
+--profile FPL-MAS9-NONPROD
